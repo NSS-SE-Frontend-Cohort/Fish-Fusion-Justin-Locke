bb163725f941b2e641b280f3d1f25cad7c3c095d
Justin Locke <Justindeanlocke@gmail.com>
2024-12-05 14:36:10 -0600

    Code complete again

--- restaurant.js
+++ restaurant.js
@@ -6,17 +6,23 @@ const fishMenu = (pricePoint) => {
 
     const todaysFish = mongerInventory(pricePoint);
 
-    let menuHTML = `<h1>Menu</h1>`;
+    let menuHTML = 
+    `<h1>Menu</h1>
+    
+<article class="menu">`
+;
 
     todaysFish.forEach(fish => {
         menuHTML += `
-        <article class="menu">
             <h2>${fish.species}</h2>
             <section class="menu__item">${fish.species} Soup</section>
             <section class="menu__item">${fish.species} Sandwich</section>
             <section class="menu__item">Grilled ${fish.species}</section>
-        </article>`;
+
+            `
     });
+    
+    menuHTML += `</article>`
 
     return menuHTML;
 
