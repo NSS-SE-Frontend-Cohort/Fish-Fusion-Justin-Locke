972fa26cc9d764d9032db377b11b4fd84f68ee59
Justin Locke <Justindeanlocke@gmail.com>
2024-12-06 12:35:26 -0600

    Updated html format to remove redundant spaces.

--- restaurant.js
+++ restaurant.js
@@ -6,24 +6,22 @@ const fishMenu = (pricePoint) => {
 
     const todaysFish = mongerInventory(pricePoint);
 
-    let menuHTML = 
-    `<h1>Menu</h1>
-    
-<article class="menu">`
-;
+    let menuHTML = `<h1>Menu</h1>
 
+<article class="menu">`;
+
+console.log(todaysFish)
     todaysFish.forEach(fish => {
         menuHTML += `
-            <h2>${fish.species}</h2>
-            <section class="menu__item">${fish.species} Soup</section>
-            <section class="menu__item">${fish.species} Sandwich</section>
-            <section class="menu__item">Grilled ${fish.species}</section>
-
-            `
+<h2>${fish.species}</h2>
+    <section class="menu__item">${fish.species} Soup</section>
+    <section class="menu__item">${fish.species} Sandwich</section>
+    <section class="menu__item">Grilled ${fish.species}</section>`
     });
     
     menuHTML += `</article>`
 
+    console.log(menuHTML)
     return menuHTML;
 
 }
